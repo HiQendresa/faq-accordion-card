87189504f4e39b999139085dff84a9aa50e74af9
Qendresa Braha <hiqendresa@gmail.com>
2023-11-03 01:13:08 +0100

    mobile view fixed

--- script.js
+++ script.js
@@ -2,29 +2,16 @@ const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach((item) => {
     const btn = item.querySelector('.question');
-    const answer = item.querySelector('.answer');
-    const icon = item.querySelector('.question img');
-    const question = item.querySelector('.question h3');
-    
     btn.addEventListener('click', () => {
-        faqs.forEach((faqItem) => {
-            const faqAnswer = faqItem.querySelector('.answer');
-            if (faqAnswer !== answer) {
-                faqAnswer.style.display = 'none';
-                icon.style.transform = "rotate(0deg)";
-                question.style.fontWeight = 'regular';
-            }
-        });
-      if (answer.style.display === 'none' || answer.style.display === '') {
-                answer.style.display = 'block';
-                icon.style.transform = "rotate(180deg)";
-                question.style.fontWeight = 'bold';
-            } else {
-                answer.style.display = 'none';
-                icon.style.transform = "rotate(0deg)";
-                question.style.fontWeight = 'regular';
-            }
-        });
-})
+        
+        const isActive = item.classList.contains('active');
 
+        faqs.forEach((faq) => {
+            faq.classList.remove('active');
+        });
 
+        if (!isActive) {
+            item.classList.add('active');
+        }
+    });
+});
